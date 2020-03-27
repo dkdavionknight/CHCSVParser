8494bac422da2e03a0c832d96aa32bb0e5cb6b5d
Davion Knight <dkdavionknight@gmail.com>
2020-03-27 16:03:37 +0800

    Added support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "CHCSVParser",
+    products: [
+        .library(
+            name: "CHCSVParser",
+            targets: ["CHCSVParser"])
+    ],
+    targets: [
+        .target(
+            name: "CHCSVParser",
+            dependencies: [],
+            path: "CHCSVParser",
+            sources: ["CHCSVParser"],
+            publicHeadersPath: "include")
+    ]
+)
